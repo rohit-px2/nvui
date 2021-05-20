4fd202cd163042ea14057d64ebd689d91033295d
Rohit Pradhan <rohit.px02@gmail.com>
2021-05-19 21:32:26 -0400

    Show message if Neovim not found in PATH

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
 #include <QMetaObject>
 #include <QMetaType>
+#include <QMessageBox>
+#include <QStyleFactory>
+#include <QStringBuilder>
 #include <string>
 #include <vector>
 #include <thread>
@@ -9,7 +12,7 @@
 #include "nvim.hpp"
 #include "window.hpp"
 #include <msgpack.hpp>
-#include <QStyleFactory>
+
 using std::string;
 using std::vector;
 
@@ -26,14 +29,24 @@ Q_DECLARE_METATYPE(msgpack::object)
 int main(int argc, char** argv)
 {
   qRegisterMetaType<msgpack::object>();
-  std::ios_base::sync_with_stdio(false);
-  const auto args = get_args(argc, argv);
+  //const auto args = get_args(argc, argv);
   QApplication app {argc, argv};
-  const auto nvim = std::make_shared<Nvim>();
-  Window w {nullptr, nvim};
-  w.register_handlers();
-  w.show();
-  nvim->set_var("nvui", 1);
-  nvim->attach_ui(DEFAULT_ROWS, DEFAULT_COLS);
-  return app.exec();
+  try
+  {
+    const auto nvim = std::make_shared<Nvim>();
+    Window w {nullptr, nvim};
+    w.register_handlers();
+    w.show();
+    nvim->set_var("nvui", 1);
+    nvim->attach_ui(DEFAULT_ROWS, DEFAULT_COLS);
+    return app.exec();
+  }
+  catch (const std::exception& e)
+  {
+    // The main purpose is to catch the exception where Neovim is not
+    // found in PATH
+    QMessageBox msg;
+    msg.setText("Error occurred: " % QLatin1String(e.what()) % ".");
+    msg.exec();
+  }
 }

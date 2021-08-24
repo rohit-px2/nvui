fea4350351d0195ace0c7e37306938ea3b243f93
Rohit Pradhan <rohit.px02@gmail.com>
2021-08-23 22:24:08 -0400

    Add powershell scripts, use actions/cache directly

---
+++ scripts/windows/build-debug.ps1
@@ -0,0 +1,2 @@
+cmd /C "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G Ninja"
+cmake --build build
---
+++ scripts/windows/test.ps1
@@ -0,0 +1,2 @@
+cmake --build build --target nvui_test
+./build/nvui_test

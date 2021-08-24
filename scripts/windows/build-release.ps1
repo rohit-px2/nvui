03ad4044932d25dbf976e2c608fa6cda30368499
Rohit Pradhan <rohit.px02@gmail.com>
2021-08-24 17:04:00 -0400

    Allow adding compilers and generators as paramters in ps1 script

--- scripts/windows/build-release.ps1
+++ scripts/windows/build-release.ps1
@@ -1,2 +1,9 @@
-cmd /C "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G Ninja"
+param(
+	[string]$cc = "clang",
+	[string]$cxx = "clang++",
+	[string]$gen = "Ninja"
+)
+$generator = '"{0}"' -f $gen
+$cmd = Write-Output "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator"
+cmd /C $cmd
 cmake --build build

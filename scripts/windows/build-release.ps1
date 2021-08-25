f0d11b151fc17361d6f45916785a2c688e53f89f
Rohit Pradhan <rohit.px02@gmail.com>
2021-08-24 21:36:21 -0400

    Cache to one key for Debug & Release, use config in cmake build

--- scripts/windows/build-release.ps1
+++ scripts/windows/build-release.ps1
@@ -6,4 +6,4 @@ param(
 $generator = '"{0}"' -f $gen
 $cmd = Write-Output "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator"
 cmd /C $cmd
-cmake --build build
+cmake --build build --config Release

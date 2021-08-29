2ab3ebe8bb97bc2d70b302944af2bf0ffd137324
Rohit Pradhan <rohit.px02@gmail.com>
2021-08-28 22:41:03 -0400

    Remove display assets in packaged build

--- scripts/linux/package.sh
+++ scripts/linux/package.sh
@@ -5,4 +5,5 @@ mkdir packaged
 mkdir packaged/bin
 cp nvui packaged/bin
 cp -r ../assets packaged
+rm -rf packaged/assets/display
 cp -r ../vim packaged
--- scripts/windows/package.ps1
+++ scripts/windows/package.ps1
@@ -8,6 +8,7 @@ cd ..
 Copy-Item -Force -Recurse -Path plugins -Destination nvui/bin
 Copy-Item -Force -Recurse -Path ../assets -Destination nvui
 Copy-Item -Force -Recurse -Path ../vim -Destination nvui
+Remove-Item -Force -Recurse -Path nvui/assets/display
 Compress-Archive -Force -Path nvui -DestinationPath nvui.zip
 Move-Item -Force -Path nvui.zip -Destination ../
 cd ..

45b8d586aedef0c79481059d167b775ef8151710
Rohit Pradhan <rohit.px02@gmail.com>
2021-09-08 23:48:20 -0400

    Don't compile with boost asio coroutines on Windows

--- scripts/linux/build-release.sh
+++ scripts/linux/build-release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cmake -B build . \
 	-DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake \
-	-DCMAKE_BUILD_TYPE=Release
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_TEST=OFF
 cmake --build build --config Release
--- scripts/windows/build-release.ps1
+++ scripts/windows/build-release.ps1
@@ -1,9 +1,10 @@
 param(
 	[string]$cc = "clang",
 	[string]$cxx = "clang++",
-	[string]$gen = "Ninja"
+	[string]$gen = "Ninja",
+	[string]$cmake_args = ""
 )
 $generator = '"{0}"' -f $gen
-$cmd = Write-Output "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator"
+$cmd = Write-Output "cmake . -B build -DBUILD_TEST=OFF -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator $cmake_args"
 cmd /C $cmd
 cmake --build build --config Release

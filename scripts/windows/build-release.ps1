07586699578d0fc6f9e497e089383074aeb07b8f
Rohit Pradhan <rohit.px02@gmail.com>
2022-11-14 20:36:25 -0500

    Try removing vcpkg.json and manually installing vcpkg packages

--- scripts/windows/build-release.ps1
+++ scripts/windows/build-release.ps1
@@ -5,5 +5,7 @@ param(
 )
 $generator = '"{0}"' -f $gen
 $cmd = Write-Output "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator"
+.\vcpkg\bootstrap-vcpkg.bat -disableMetrics
+.\vcpkg\vcpkg install fmt msgpack qt5-base qt5-svg boost-process boost-container
 cmd /C $cmd
 cmake --build build --config Release

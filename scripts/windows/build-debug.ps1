764f9b71b4a87112d99fb3813434623f37b8fb17
Rohit Pradhan <rohit.px02@gmail.com>
2022-11-16 20:34:28 -0500

    Hopefully fix actions

--- scripts/windows/build-debug.ps1
+++ scripts/windows/build-debug.ps1
@@ -5,7 +5,5 @@ param(
 )
 $generator = '"{0}"' -f $gen
 $cmd = Write-Output "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator -DBUILD_TESTS=ON"
-.\vcpkg\bootstrap-vcpkg.bat -disableMetrics
-.\vcpkg\vcpkg install fmt msgpack boost-process boost-container qt5-base qt5-svg catch2
 cmd /C $cmd
 cmake --build build --config Debug

c9b1e9ff97537af2eb09964dd8b9a0a3de4e48b6
Rohit Pradhan <rohit.px02@gmail.com>
2023-01-19 02:03:46 -0500

    Remove part of CMAKE_PREFIX_PATH

--- scripts/windows/build-debug.ps1
+++ scripts/windows/build-debug.ps1
@@ -4,6 +4,6 @@ param(
 	[string]$gen = "Ninja"
 )
 $generator = '"{0}"' -f $gen
-$cmd = Write-Output "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator -DBUILD_TESTS=ON -DCMAKE_PREFIX_PATH=""$env:CMAKE_PREFIX_PATH"""
+$cmd = Write-Output "cmake . -B build -DCMAKE_PREFIX_PATH=""$env:CMAKE_PREFIX_PATH"" -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator -DBUILD_TESTS=ON"
 cmd /C $cmd
 cmake --build build --config Debug
--- scripts/windows/build-release.ps1
+++ scripts/windows/build-release.ps1
@@ -4,6 +4,6 @@ param(
 	[string]$gen = "Ninja"
 )
 $generator = '"{0}"' -f $gen
-$cmd = Write-Output "cmake . -B build -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator -DCMAKE_PREFIX_PATH=""$env:CMAKE_PREFIX_PATH"""
+$cmd = Write-Output "cmake . -B build -DCMAKE_PREFIX_PATH=""$env:CMAKE_PREFIX_PATH"" -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator"
 cmd /C $cmd
 cmake --build build --config Release

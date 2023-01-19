e048dc536a4fd599e23f473e93307427440da082
Rohit Pradhan <rohit.px02@gmail.com>
2023-01-19 02:52:27 -0500

    More dependencies for Ubuntu, try to use Qt5_Dir on Windows

--- scripts/windows/build-debug.ps1
+++ scripts/windows/build-debug.ps1
@@ -4,6 +4,6 @@ param(
 	[string]$gen = "Ninja"
 )
 $generator = '"{0}"' -f $gen
-$cmd = Write-Output "cmake . -B build -DCMAKE_PREFIX_PATH=""$env:CMAKE_PREFIX_PATH"" -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator -DBUILD_TESTS=ON"
+$cmd = Write-Output "cmake . -B build -DQt5_DIR=$env:Qt5_DIR -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$cc -DCMAKE_CXX_COMPILER=$cxx -G $generator -DBUILD_TESTS=ON"
 cmd /C $cmd
 cmake --build build --config Debug
